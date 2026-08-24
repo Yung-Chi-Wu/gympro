048edf3370f6ca60d1d458060fc4fa44cdfe6f64
Yung-Chi-Wu <wu,yung@northeastern.edu>
2026-08-24 15:53:06 -0400

    Fix dark mode globally via CSS variable override, add theme toggle to all auth pages

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,18 +1,23 @@
 import { getLocale } from 'next-intl/server'
 import { LangToggle } from '@/components/LangToggle'
 import { SignupForm } from '@/components/SignupForm'
+import { ThemeToggle } from '@/components/ThemeToggle'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 export default async function SignupPage() {
   const locale = await getLocale()
 
   return (
-    <div className="flex min-h-screen flex-col">
-      <div className="flex justify-end p-4">
-        <LangToggle currentLocale={locale} />
+    <ThemeProvider>
+      <div className="flex min-h-screen flex-col bg-[#FAFAF8] dark:bg-[#1A1814]">
+        <div className="flex justify-end items-center gap-3 p-4">
+          <ThemeToggle />
+          <LangToggle currentLocale={locale} />
+        </div>
+        <div className="flex flex-1 items-center justify-center px-4">
+          <SignupForm locale={locale} />
+        </div>
       </div>
-      <div className="flex flex-1 items-center justify-center px-4">
-        <SignupForm locale={locale} />
-      </div>
-    </div>
+    </ThemeProvider>
   )
 }
